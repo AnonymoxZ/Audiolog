c1f065395d44720c34eb121ef4f89aa1fdb57427
Vitor <areazeroac@gmail.com>
2024-09-17 02:00:08 -0300

    Segue mods

--- _src/dwd_mps.py
+++ _src/dwd_mps.py
@@ -4,21 +4,22 @@ from pytubefix.cli import on_progress
 from settings import *
 
 # Diretorio de saída de download
-musicas = 'C:\\Users\\Vitor\\OneDrive\\Área de Trabalho\\projetos\\Audiolog\\musicas'
+musicas = 'C:\\Users\\Vitor\\OneDrive\\Área de Trabalho\\musicas'
 videos = 'C:\\Users\\User\\OneDrive\\Área de Trabalho\\videos'
 
 def BaixarMP3(link):
-    audio = YouTube(link)
-    musica = audio.streams.get_audio_only()
-    musica.download(mp3=True)
-    NoteSucess(audio.title)
+    try:
+        audio = YouTube(link,on_progress_callback = on_progress)
+        musica = audio.streams.get_audio_only()
+        musica.download(mp3=True)
+        NoteSucess(audio.title)
+    except:
+        NoteError()
 
 def BaixarMP4(link):
     try:
         video = YouTube(link)
         vd = video.streams.get_highest_resolution()
-        vd.download(output_path=videos)
+        vd.download()
     except:
         NoteError()
-
-BaixarMP3('https://youtube.com/shorts/Uw6WuuFOwlk?si=nsbiJuCXhNoog62c')
